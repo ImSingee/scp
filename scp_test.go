2bf45b859bb20ff542bff641817f70fa2338fd1e
Singee <git@singee.me>
2020-12-10 16:54:37 +0800

    add new test (TestCopyFileToFolder)

--- scp_test.go
+++ scp_test.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"github.com/ImSingee/tt"
 	"os"
-	"path"
+	"path/filepath"
 	"testing"
 	"time"
 )
 
 func generateTempFile(t *testing.T, data string) string {
-	p := path.Join(os.TempDir(), fmt.Sprintf("scp-test-%d", time.Now().UnixNano()))
+	p := filepath.Join(os.TempDir(), fmt.Sprintf("scp-test-%d", time.Now().UnixNano()))
 
 	f, err := os.Create(p)
 	tt.AssertIsNil(t, err)
@@ -35,4 +35,21 @@ func TestCopyFileToFile(t *testing.T) {
 
 	tt.AssertTrue(t, checkFileExist(t, "/test/test01"))
 	tt.AssertEqual(t, "abcdefg", string(readFile(t, "/test/test01")))
+}
+
+func TestCopyFileToFolder(t *testing.T) {
+	reset(t)
+
+	session := getSshSession(t)
+	defer session.Close()
+
+	p := generateTempFile(t, "abcdefg")
+
+	err := Copy(session, p, "/test")
+	tt.AssertIsNil(t, err)
+
+	remotePath := fmt.Sprintf("/test/%s", filepath.Base(p))
+
+	tt.AssertTrue(t, checkFileExist(t, remotePath))
+	tt.AssertEqual(t, "abcdefg", string(readFile(t, remotePath)))
 }
